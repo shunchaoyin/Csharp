cc8b7be52decc5f4338ce2d1d8f2fdf04369af66
shunchao <shunchaoyin@gmail.com>
2025-02-25 21:54:59 +0800

    add MyToDo and WebApi projects.

---
+++ MyToDo/MyToDo/MyToDo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyToDo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+
+        }
+    }
+
+}
