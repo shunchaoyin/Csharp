6fb203cc65e39346eece2b1ae2c82f73a1f45a3d
shunchao <shunchaoyin@gmail.com>
2025-03-12 21:33:53 +0800

    add myapp

---
+++ MyApp/Plugin.UI/Model/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.UI.Model
+{
+    public enum LogType
+    {
+        Info,
+        Warning,
+        Error
+    }
+    class Log
+    {
+        public LogType Type { get; set; }
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+        public Log(LogType type, string message)
+        {
+            Type = type;
+            Message = message;
+            Time = DateTime.Now;
+        }
+    }
+}
---
+++ MyApp/Plugin.UI/ViewModel/LogViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.Collections;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using Plugin.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.UI.ViewModel
+{
+    partial class LogViewModel : ObservableObject, IRecipient<ValueChangedMessage<Log>>
+    {
+
+        public LogViewModel()
+        {
+            Logs = new ObservableCollection<Log>();
+            Logs.Add(new Log(LogType.Info, "This is a test log."));
+            Logs.Add(new Log(LogType.Warning, "This is a test warning."));
+            Logs.Add(new Log(LogType.Error, "This is a test error."));
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<Log>? logs;
+
+        public void Receive(ValueChangedMessage<Log> message)
+        {
+            Logs?.Add(message.Value);
+        }
+    }
+}
