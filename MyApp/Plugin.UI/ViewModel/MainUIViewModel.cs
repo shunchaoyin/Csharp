38a629f946bbbf500db929d68c6c0172e069d221
shunchao <shunchaoyin@gmail.com>
2025-03-12 23:44:21 +0800

    add handycontrol

--- MyApp/Plugin.UI/ViewModel/MainUIViewModel.cs
+++ MyApp/Plugin.UI/ViewModel/MainUIViewModel.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Plugin.UI.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Plugin.UI.ViewModel
 {
-    class MainUIViewModel
+    partial class MainUIViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private UserControl? selectedView;
+
+        public MainUIViewModel()
+        {
+            NavigateCommand = new RelayCommand<string?>(Navigate);
+        }
+
+        public RelayCommand<string?> NavigateCommand { get; }
+
+        private void Navigate(string? viewName)
+        {
+            switch (viewName)
+            {
+                case "Action1":
+                    //SelectedView = new ActionsView();
+                    break;
+                case "Action2":
+                    // 这里可以添加其他视图
+                    break;
+                default:
+                    //SelectedView = null;
+                    break;
+            }
+        }
     }
 }
